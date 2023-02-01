3065372d7d7ea1e040037e2d6b261b53515c4ef7
tutorcs <tutorcs@163.com>
2023-02-01 12:35:26 +0800

    WeChat cstutorcs CS7637 KBAI Mini Project 5 MonsterDiagnosisAgent

---
+++ MonsterDiagnosisAgent.py
@@ -0,0 +1,28 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+class MonsterDiagnosisAgent:
+    def __init__(self):
+        # If you want to do any initial processing, add it here.
+        pass
+
+    def solve(self, diseases, patient):
+        # Add your code here!
+        #
+        # The first parameter to this method is a list of diseases, represented as a
+        # list of 2-tuples. The first item in each 2-tuple is the name of a disease. The
+        # second item in each 2-tuple is a dictionary of symptoms of that disease, where
+        # the keys are letters representing vitamin names ("A" through "Z") and the values
+        # are "+" (for elevated), "-" (for reduced), or "0" (for normal).
+        #
+        # The second parameter to this method is a particular patient's symptoms, again
+        # represented as a dictionary where the keys are letters and the values are
+        # "+", "-", or "0".
+        #
+        # This method should return a list of names of diseases that together explain the
+        # observed symptoms. If multiple lists of diseases can explain the symptoms, you
+        # should return the smallest list. If multiple smallest lists are possible, you
+        # may return any sufficiently explanatory list.
+        pass
+
---
+++ README.md
@@ -0,0 +1,12 @@
+# CS7637 KBAI Mini Project 5 MonsterDiagnosisAgent
+# WeChat: cstutorcs
+
+# QQ: 749389476
+
+# Email: tutorcs@163.com
+
+# Computer Science Tutor
+
+# Programming Help
+
+# Assignment Project Exam Help
